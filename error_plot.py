9fa6488f3e2c205de1df20106e98898b6bde05dd
zethdubois <zethdubois@pm.me>
2023-02-27 16:54:38 -0800

    added arg for error_plot

--- error_plot.py
+++ error_plot.py
@@ -1,7 +1,9 @@
 from matplotlib import pyplot as plt
 import csv
+import sys
+prefix=sys.argv[-1]
 
-with open('execute.log') as fp:
+with open(f'{prefix}_execute.log') as fp:
     execute_rdr = csv.DictReader(fp, fieldnames='k,time,error,register,target'.split(','))
 
     for i, d in enumerate(execute_rdr):
@@ -11,7 +13,7 @@ with open('execute.log') as fp:
         if d['error'].lower() == 'false':
             plt.axvline((t - t0) * 1e-9)
 
-with open('key.log') as fp:
+with open(f'{prefix}_key.log') as fp:
     key_rdr = csv.DictReader(fp, fieldnames='k,time,bit,error,register,target'.split(','))
 
     for i, d in enumerate(key_rdr):
